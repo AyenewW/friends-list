e598c3d3b4f6acaa4df9afb5c01977243b80bbe9
AYENEW WALLE <ayeshambel@gmail.com>
2022-12-21 20:28:56 -0500

    y first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Wrapper from "./components/Wrapper/Wrapper";
+import Title from "./components/Title";
+import FriendCard from "./components/FriendCard";
+import React from "react";
+import imageOne from "./images/image_one.jpg"
+import imageTwo from "./images/image_two.jpg"
+import imageThree from "./images/image_three.jpg"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+  <Title/>
+  <Wrapper>
+  <FriendCard image={imageOne}
+   name="Ayenew"
+   occupation="front"
+   location="Dc"/>
+ 
+  <FriendCard image={imageTwo} 
+  name="Zelalem"
+  occupation="back"
+  location="va"/>
+
+  <FriendCard image={imageThree}
+  name="Lilisa"
+  occupation="both"
+  location="mch"/>
+  
+  </Wrapper> 
+  </div>
   );
 }
 
